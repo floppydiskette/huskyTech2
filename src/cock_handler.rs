b15dff4dc48a5482b1ae836c3f36f861b4c2daca
husky <husky@gaycatboy.sex>
2022-05-04 00:09:48 -0700

    yeah

--- src/cock_handler.rs
+++ src/cock_handler.rs
@@ -1,10 +1,63 @@
 // file for handling cocks
 
+use crate::Thingy;
+
 trait CockElement {
     fn get_x(&self) -> i32;
     fn get_y(&self) -> i32;
     fn get_width(&self) -> i32;
     fn get_height(&self) -> i32;
+}
+
+pub struct CockBlock { // a collection of cock elements
+    x: i32,
+    y: i32,
+    cached_width: i32,
+    cached_height: i32,
+    elements: Vec<CockElement>,
+}
+
+impl Thingy for CockBlock {
+    fn get_x(&self) -> i32 {
+        self.x
+    }
+
+    fn get_y(&self) -> i32 {
+        self.y
+    }
+
+    fn get_z(&self) -> i32 {
+        0
+    }
+
+    fn get_width(&self) -> i32 {
+        self.cached_width
+    }
+
+    fn get_height(&self) -> i32 {
+        self.cached_height
+    }
+
+    fn get_depth(&self) -> i32 {
+        0
+    }
+}
+
+impl CockElement for CockBlock {
+    fn get_x(&self) -> i32 {
+        self.x
+    }
+
+    fn get_y(&self) -> i32 {
+        self.y
+    }
+
+    fn get_width(&self) -> i32 {
+        self.cached_width
+    }
+    fn get_height(&self) -> i32 {
+        self.cached_height
+    }
 }
 
 pub struct CockBasicText {
@@ -31,4 +84,31 @@ impl CockElement for CockBasicText {
         // character height is 8 pixels, so return 8
         8
     }
+}
+
+impl CockBasicText {
+    pub fn new(x: i32, y: i32, text: String) -> CockBasicText {
+        CockBasicText {
+            x,
+            y,
+            text,
+        }
+    }
+
+    pub fn set_text(&mut self, text: String) {
+        self.text = text;
+    }
+
+    pub fn get_text(&self) -> String {
+        self.text.clone()
+    }
+}
+
+pub fn parse_cock(cock: &str) -> Result<CockElement, String> {
+    // if the line begins with two forward slashes, it's a comment
+    if cock.starts_with("//") {
+        Err("comment".to_string())
+    }
+
+    Err("no cock ):".to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+pub trait Thingy {
+    fn get_x(&self) -> i32;
+    fn get_y(&self) -> i32;
+    fn get_z(&self) -> i32;
+    fn get_width(&self) -> i32;
+    fn get_height(&self) -> i32;
+    fn get_depth(&self) -> i32;
+}
+
 pub mod renderer;
 pub mod helpers;
 pub mod cock_handler;
+pub mod animation;
 
 fn main() {
     println!("good day! initialising huskyTech2");
