3675472438275476048a6b56aafd24ba84b65127
husky <husky@gaycatboy.sex>
2022-05-18 19:13:53 -0700

    kinda work now!

--- src/sunlust_intro/mod.rs
+++ src/sunlust_intro/mod.rs
@@ -6,28 +6,69 @@ use std::ffi::CString;
 use std::ptr::null;
 use std::time::SystemTime;
 use dae_parser::Document;
-use gfx_maths::Vec2;
+use gfx_maths::{Vec2, Vec3};
 use kira::manager::AudioManager;
 use kira::manager::backend::cpal::CpalBackend;
 use kira::sound::static_sound::{StaticSoundData, StaticSoundSettings};
 use libsex::bindings::*;
-use crate::animation::Animation2D;
+use crate::animation::Animation;
 use crate::helpers::gen_rainbow;
 use crate::renderer::{Colour, ht_renderer};
 
 pub fn animate(renderer: &mut ht_renderer, sss: &mut AudioManager<CpalBackend>) {
     // load rainbow shader
-    let rainbow_shader = renderer.load_shader("rainbow").expect("failed to load rainbow shader");
+    let rainbow_shader = renderer.load_shader("basic").expect("failed to load rainbow shader");
     // load ht2 logo model
     let document = Document::from_file("base/models/ht2.dae").expect("failed to load dae file");
-    let mesh = renderer.initMesh(document, "Cube_001-mesh", rainbow_shader).unwrap();
+    let mut mesh = renderer.initMesh(document, "Cube_001-mesh", rainbow_shader).unwrap();
 
     let mut sunlust_sfx = StaticSoundData::from_file("base/snd/sunlust.wav", StaticSoundSettings::default()).expect("failed to load sunlust.wav");
     sss.play(sunlust_sfx.clone());
     println!("playing sunlust.wav");
     let time_of_start = SystemTime::now(); // when the animation started
+    let mut current_time = SystemTime::now(); // for later
+    let rainbow_time = 900.0; // in milliseconds
+    let rainbow_anim = Animation::new(Vec3::new(0.0, 0.0, -10.0), Vec3::new(0.0, 0.25, 2.0), rainbow_time);
 
     loop {
+        // check how long it's been
+        current_time = SystemTime::now();
+        let time_since_start = current_time.duration_since(time_of_start).expect("failed to get time since start");
+        let time_since_start = time_since_start.as_millis() as f32;
+        // has it been long enough?
+        if time_since_start > rainbow_time {
+            break;
+        }
+        // get the point at the current time
+        let point = rainbow_anim.get_point_at_time(time_since_start as f64);
+        // set the position of the mesh
+        mesh.position = point;
+        // draw the mesh
+        renderer.render_mesh(mesh, rainbow_shader, true);
+        // swap buffers
+        renderer.swap_buffers();
+    }
 
+    let normal_time = 9119.0 - rainbow_time; // in milliseconds
+    let normal_anim = Animation::new(Vec3::new(0.0, 0.25, 2.0), Vec3::new(0.0, 0.35, 1.0), normal_time);
+
+    loop {
+        // check how long it's been
+        current_time = SystemTime::now();
+        let time_since_start = current_time.duration_since(time_of_start).expect("failed to get time since start");
+        let time_since_start = time_since_start.as_millis() as f32;
+        // has it been long enough?
+        if time_since_start > normal_time {
+            break;
+        }
+        let time_since_start =  time_since_start + rainbow_time;
+        // get the point at the current time
+        let point = normal_anim.get_point_at_time(time_since_start as f64);
+        // set the position of the mesh
+        mesh.position = point;
+        // draw the mesh
+        renderer.render_mesh(mesh, rainbow_shader, false);
+        // swap buffers
+        renderer.swap_buffers();
     }
 }
